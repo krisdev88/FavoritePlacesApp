e887cbb4239cbe366400808cf693c7015a2feb4f
krisdev88 <kceleban09@gmail.com>
2023-06-29 08:29:43 +0200

    Add Navigator around Screens: DetailScreen, PlacesList, AddPlace. Add riverpod and provider to manage an input date

--- lib/colorsApp.dart
+++ lib/colorsApp.dart
@@ -1,11 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
 
-// class ColorsApp {
-//   static const seedColorApp = Color.fromARGB(255, 102, 6, 247);
-//   static const backgroundApp = Color.fromARGB(255, 56, 49, 66);
-// }
-
 final colorScheme = ColorScheme.fromSeed(
   brightness: Brightness.dark,
   seedColor: const Color.fromARGB(255, 102, 6, 247),
