45b38b166029f3641f6ea2cfa0e4515c3d06cdbc
krisdev88 <kceleban09@gmail.com>
2023-06-30 21:59:12 +0200

    Add "Get Current Location"  function

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
+import 'package:favorite_places_app/screens/places.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-import 'package:favorite_places_app/screens/places.dart';
 import 'colorsApp.dart';
 
-void main() => runApp(const ProviderScope(child: MyApp()));
+Future main() async {
+  await dotenv.load(fileName: ".env");
+  runApp(const ProviderScope(child: MyApp()));
+}
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
